74d4dc79722db80218109431e9835b2cafa71ecd
Aswoo <yhnu191919@gmail.com>
2017-09-26 23:19:58 +0900

    BoxStore Activity TEST App

---
+++ Android/XML_TEST/app/src/main/java/com/aswoo/android/xml_test/MainActivity.java
@@ -0,0 +1,99 @@
+
+package com.aswoo.android.xml_test;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.aswoo.android.xml_test.fragement.*;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        BottomNavigationViewHelper bottomnavigationViewHelper = new BottomNavigationViewHelper();
+        bottomnavigationViewHelper.removeShiftMode(bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                selectedFragment = fragment_home.newInstance();
+                                break;
+                            case R.id.navigation_category:
+                                selectedFragment = fragment_category.newInstance();
+                                break;
+                            case R.id.navigation_sell:
+                                selectedFragment = fragment_sell.newInstance();
+                                break;
+                            case R.id.navigation_notifications:
+                                selectedFragment = fragment_notification.newInstance();
+                                break;
+                            case R.id.navigation_mypage:
+                                selectedFragment = fragment_mypage.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment_home.newInstance());
+        transaction.commit();
+
+
+
+        //Used to select an item programmatically
+
+    }
+    class BottomNavigationViewHelper {
+        public void removeShiftMode(BottomNavigationView view) {
+            BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+            try {
+                Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+                shiftingMode.setAccessible(true);
+                shiftingMode.setBoolean(menuView, false);
+                shiftingMode.setAccessible(false);
+                for (int i = 0; i < menuView.getChildCount(); i++) {
+                    BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                    item.setShiftingMode(false);
+                    // set once again checked value, so view will be updated
+                    item.setChecked(item.getItemData().isChecked());
+                }
+            } catch (NoSuchFieldException e) {
+                Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field");
+            } catch (IllegalAccessException e) {
+                Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode");
+            }
+        }
+    }
+
+}
---
+++ Android/XML_TEST/app/src/main/java/com/aswoo/android/xml_test/fragement/fragment_home.java
@@ -0,0 +1,31 @@
+package com.aswoo.android.xml_test.fragement;
+
+/**
+ * Created by seungwoo on 2017-09-25.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aswoo.android.xml_test.R;
+
+public class fragment_home extends Fragment {
+
+
+    public static fragment_home newInstance(){
+        fragment_home fragment_home = new fragment_home();
+        return fragment_home;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+}
